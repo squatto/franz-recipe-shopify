d47c4497818c733116b455342b5dc1803ad224a1
Scott Carpenter <scott@payforstay.com>
2020-09-16 20:30:31 -0600

    Check if we're logged out and - if so - click the account link to log back in

--- webview.js
+++ webview.js
@@ -8,6 +8,35 @@ module.exports = Franz => {
     });
   }
 
+  // have we clicked an account link already?
+  let accountLinkClicked = false;
+
+  const checkIfLoggedOut = () => {
+    // check for a heading that says "Choose an account"
+    // if it's present, we are logged out and Shopify is asking us to select our account to log back in
+    const heading = document.querySelector('h1.ui-heading');
+
+    if (heading !== undefined && heading.textContent === 'Choose an account') {
+      // we're logged out and the account selection screen is showing
+      console.log('Logged out');
+      console.log('Checking for an account to log into');
+
+      // find the first account link
+      const link = document.querySelector('a.account-picker__item');
+
+      if (link !== undefined) {
+        // click the account link to log us back in
+        console.log('Found an account to log into');
+        console.log('Clicking the account link...');
+
+        accountLinkClicked = true;
+        link.click();
+      } else {
+        console.warn('Unable to find an account to log into!');
+      }
+    }
+  };
+
   const getMessages = function getMessages() {
     let unread = 0;
 
@@ -26,6 +55,10 @@ module.exports = Franz => {
     }
 
     Franz.setBadge(parseInt(unread, 10));
+
+    if (! accountLinkClicked) {
+      checkIfLoggedOut();
+    }
   };
 
   Franz.loop(getMessages);
